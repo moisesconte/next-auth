655eff69fe61a434e330a13f99a53f6ee8805a60
mtfc.dk <mtfc.dk@hotmail.com>
2021-11-22 21:37:04 -0300

    main

---
+++ pages/dashboard.tsx
@@ -0,0 +1,18 @@
+import { useContext, useEffect } from "react"
+import { AuthContext } from "../contexts/AuthContex"
+import { api } from "../services/api"
+
+export default function Dashboard() {
+    const { user } = useContext(AuthContext)
+
+    useEffect(() => {
+        api.get('/me').then(response => console.log(response))
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
+
+    return (
+        <h1>Dashboard: {user?.email}</h1>
+    )
+}
